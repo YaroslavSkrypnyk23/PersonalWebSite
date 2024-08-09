1cc8c50c705de6a9467d4c4c10c25822cddb83d1
YaroslavSkrypnyk23 <yaroslavskrypnyk22@ukr.net>
2024-08-09 17:40:39 +0300

    Fixed problems in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PersonalWebSite/',  // Замість 'repository-name' вкажіть ім'я вашого репозиторію на GitHub
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js', // Зберігає ваш основний JavaScript файл в кореневій папці dist
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'index.css') {
+            return 'index.css'; // Зберігає CSS файл в кореневій папці dist
+          }
+          return 'assets/[name].[ext]'; // Інші статичні файли в папці assets
+        },
+      },
+    },
+  },
 })
